12d614e7549670047d2deba52ab33cdb273ae5c0
thatlittlegit <personal@thatlittlegit.tk>
2020-07-08 23:10:42 -0400

    Use subprocess.run() instead of subprocess.call() in beautify.py

--- build-aux/beautify.py
+++ build-aux/beautify.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-from subprocess import call
+from subprocess import run
 from os import walk, environ, path as ospath
 
 root = environ.get('MESON_SOURCE_ROOT')
@@ -9,7 +9,7 @@ config = ospath.join(root, '.uncrustify.cfg')
 for path, dirs, files in walk(root):
     for file in files:
         if file.endswith(".vala") or file.endswith(".c"):
-            call(['uncrustify', '-q', ospath.join(path, file), '-c', config, '--replace', '--no-backup'])
+            run(['uncrustify', '-q', ospath.join(path, file), '-c', config, '--replace', '--no-backup'])
         if file.endswith(".xml") or file.endswith(".ui") or file.endswith(".xml.in"):
             filepath = ospath.join(path, file)
-            call(['xmllint', '--format', filepath, '-o', filepath])
+            run(['xmllint', '--format', filepath, '-o', filepath])
